d503f5b18631570954649eb1c9e6f8e0ed46d0de
Gabriel Dutra <gabriel-dutralima@outlook.com>
2024-12-10 13:59:50 -0300

    Melhorias de teste

--- src/main/java/com/suporte/microservico/microservico_springboot/config/SwaggerConfig.java
+++ src/main/java/com/suporte/microservico/microservico_springboot/config/SwaggerConfig.java
@@ -8,24 +8,34 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+/**
+ * Classe de configuração do Swagger para documentar e testar a API com autenticação JWT.
+ */
+@Configuration // Indica que esta classe contém configurações Spring.
 public class SwaggerConfig {
 
-    @Bean
+    /**
+     * Define as configurações do Swagger para a API.
+     *
+     * @return uma instância personalizada de OpenAPI.
+     */
+    @Bean // Declaração de um bean gerenciado pelo Spring.
     public OpenAPI customOpenAPI() {
+        // Nome do esquema de segurança utilizado para autenticação.
         final String securitySchemeName = "bearerAuth";
 
+        // Configurações principais da documentação OpenAPI.
         return new OpenAPI()
                 .info(new Info()
-                        .title("API com JWT")
-                        .version("1.0")
-                        .description("Teste de autenticação JWT via Swagger"))
-                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                        .title("API com JWT") // Título da documentação.
+                        .version("1.0") // Versão da API.
+                        .description("Teste de autenticação JWT via Swagger")) // Descrição da API.
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName)) // Requisito de segurança para autenticação.
                 .components(new Components()
                         .addSecuritySchemes(securitySchemeName, new SecurityScheme()
-                                .name(securitySchemeName)
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")));
+                                .name(securitySchemeName) // Nome do esquema de segurança.
+                                .type(SecurityScheme.Type.HTTP) // Tipo de segurança (HTTP).
+                                .scheme("bearer") // Esquema de autenticação Bearer Token.
+                                .bearerFormat("JWT"))); // Formato do token utilizado.
     }
 }
